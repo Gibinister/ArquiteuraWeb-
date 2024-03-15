05ae4ceb6044f8fe71a63dfe7407acc016bf518a
Gibinister <48371980+Gibinister@users.noreply.github.com>
2024-03-14 22:09:12 -0300

    Add files via upload

---
+++ aulaweb/src/main/java/me/gabriel/aulaweb/AulawebApplication.java
@@ -0,0 +1,13 @@
+package me.gabriel.aulaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulawebApplication.class, args);
+	}
+
+}
